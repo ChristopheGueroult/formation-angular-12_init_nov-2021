2be758e4910d699dfcaf681215a4276d0fc0c8a3
christophe <christophegueroult@gmail.com>
2021-11-16 15:14:31 +0100

    routerLink and routerLinkActive

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,14 +7,19 @@ import { UiModule } from '../ui/ui.module';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [
+    LoginModule,
+    IconsModule,
+    UiModule,
+    TemplatesModule,
     HeaderComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
   ],
-  imports: [CommonModule],
-  exports: [LoginModule, IconsModule, UiModule, TemplatesModule, HeaderComponent, NavComponent, FooterComponent],
 })
 export class CoreModule {}
