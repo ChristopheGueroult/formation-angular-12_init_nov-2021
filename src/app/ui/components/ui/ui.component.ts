6ec6cd2873714a186c63b5fc9ca9c600a728231d
christophe <christophegueroult@gmail.com>
2021-11-16 11:20:04 +0100

    attribute directive with ngClass

--- src/app/ui/components/ui/ui.component.ts
+++ src/app/ui/components/ui/ui.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ui',
   templateUrl: './ui.component.html',
-  styleUrls: ['./ui.component.scss']
+  styleUrls: ['./ui.component.scss'],
 })
 export class UiComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  /**
+   * property to open or close nav
+   */
+  public open: boolean;
+  constructor() {
+    this.open = true;
   }
 
+  ngOnInit(): void {}
+  /**
+   * methode to toggle open property
+   */
+  public toggle() {
+    this.open = !this.open;
+  }
 }
