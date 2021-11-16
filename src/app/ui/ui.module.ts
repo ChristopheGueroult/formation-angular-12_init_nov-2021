f659894411d9f8efc2987fb4166ab6b21f4910b6
christophe <christophegueroult@gmail.com>
2021-11-16 12:25:26 +0100

    basic routes

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { UiComponent } from './components/ui/ui.component';
-import { CoreModule } from '../core/core.module';
 import { Ui2Component } from './components/ui2/ui2.component';
 
 @NgModule({
   declarations: [UiComponent, Ui2Component],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [UiComponent, Ui2Component],
 })
 export class UiModule {}
