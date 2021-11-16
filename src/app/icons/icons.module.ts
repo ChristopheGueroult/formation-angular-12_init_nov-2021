003e9be06fd5185f6e5806416f32a6470dd50eb5
christophe <christophegueroult@gmail.com>
2021-11-16 09:31:27 +0100

    icons components with fontawesome

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -4,24 +4,21 @@ import { IconNavComponent } from './components/icon-nav/icon-nav.component';
 import { IconCloseComponent } from './components/icon-close/icon-close.component';
 import { IconDeleteComponent } from './components/icon-delete/icon-delete.component';
 import { IconEditComponent } from './components/icon-edit/icon-edit.component';
-
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     IconNavComponent,
     IconCloseComponent,
     IconDeleteComponent,
-    IconEditComponent
+    IconEditComponent,
   ],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [
     IconNavComponent,
     IconCloseComponent,
     IconDeleteComponent,
-    IconEditComponent
-  ]
+    IconEditComponent,
+  ],
 })
-export class IconsModule { }
+export class IconsModule {}
